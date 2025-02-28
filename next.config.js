dec91823580c6201cc8be03d6fdd3d74bf2d0c9a
Hanawahyu <hwahyutriani@gmail.com>
2025-03-01 00:03:18 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  images: {
+    unoptimized: true, // Tambahkan ini jika ada masalah dengan gambar
+  },
 };
 
 module.exports = nextConfig;
+
